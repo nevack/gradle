6eb3ff793559970abbf675b4fad8aaa668c8a0bd
Justin Van Dort <jvandort@gradle.com>
2022-05-31 17:18:21 -0400

    Make PublishArtifact to IvyArtifactName conversion lazy
    Resolving a dependency graph causes the root project to collect its metadata.
    This in turn causes all configurations to resolve their artifacts to concrete
    types. Previously, any artifacts which are backed by tasks or other lazy entities
    had their backing entities realized. However, this commit updates the conversion
    of PublishArtifacts to IvyArtifactNames to defer any actual conversion until
    data is actually accessed from the converted IvyActifactName. Often times, the data
    is not actually required during dependency resolution, and thus the underlying
    tasks will not be realized.

--- subprojects/dependency-management/src/main/java/org/gradle/internal/component/external/ivypublish/DefaultIvyModulePublishMetadata.java
+++ subprojects/dependency-management/src/main/java/org/gradle/internal/component/external/ivypublish/DefaultIvyModulePublishMetadata.java
@@ -33,7 +33,6 @@ import org.gradle.internal.component.external.model.ImmutableCapabilities;
 import org.gradle.internal.component.external.model.ModuleComponentArtifactIdentifier;
 import org.gradle.internal.component.local.model.BuildableLocalConfigurationMetadata;
 import org.gradle.internal.component.local.model.LocalFileDependencyMetadata;
-import org.gradle.internal.component.model.DefaultIvyArtifactName;
 import org.gradle.internal.component.model.ExcludeMetadata;
 import org.gradle.internal.component.model.IvyArtifactName;
 import org.gradle.internal.component.model.LocalOriginDependencyMetadata;
@@ -143,7 +142,7 @@ public class DefaultIvyModulePublishMetadata implements IvyModulePublishMetadata
     }
 
     public void addArtifact(String configuration, PublishArtifact artifact) {
-        DefaultIvyArtifactName ivyName = DefaultIvyArtifactName.forPublishArtifact(artifact);
+        IvyArtifactName ivyName = IvyArtifactName.forPublishArtifact(artifact);
         DefaultIvyModuleArtifactPublishMetadata ivyArtifact = getOrCreate(ivyName);
         ivyArtifact.setFile(artifact.getFile());
         ivyArtifact.addConfiguration(configuration);
