d704f158381ca213c612408a70f21b2130e5acda
Mikhail Lopatkin <mlopatkin@gradle.com>
2022-08-01 13:44:10 +0200

    Revert "Add callback to notify buildscan plugin about the execution phase start"
    This reverts commit 0540e2c05eb6ea106ef6e60a4f7cb1b797b076af.

--- subprojects/composite-builds/src/test/groovy/org/gradle/composite/internal/DefaultIncludedBuildRegistryTest.groovy
+++ subprojects/composite-builds/src/test/groovy/org/gradle/composite/internal/DefaultIncludedBuildRegistryTest.groovy
@@ -39,7 +39,6 @@ import org.gradle.internal.build.RootBuildState
 import org.gradle.internal.buildtree.BuildModelParameters
 import org.gradle.internal.buildtree.BuildTreeLifecycleControllerFactory
 import org.gradle.internal.buildtree.BuildTreeState
-import org.gradle.internal.enterprise.core.GradleEnterprisePluginManager
 import org.gradle.internal.event.ListenerManager
 import org.gradle.internal.operations.BuildOperationExecutor
 import org.gradle.internal.service.DefaultServiceRegistry
@@ -65,14 +64,7 @@ class DefaultIncludedBuildRegistryTest extends Specification {
     def services = new DefaultServiceRegistry()
     def modelServices = Mock(BuildModelControllerServices)
     def buildTree = Mock(BuildTreeState)
-    def factory = new BuildStateFactory(
-        buildTree,
-        listenerManager,
-        Stub(GradleEnterprisePluginManager),
-        Stub(GradleUserHomeScopeServiceRegistry),
-        Stub(CrossBuildSessionState),
-        Stub(BuildCancellationToken)
-    )
+    def factory = new BuildStateFactory(buildTree, listenerManager, Stub(GradleUserHomeScopeServiceRegistry), Stub(CrossBuildSessionState), Stub(BuildCancellationToken))
     def registry = new DefaultIncludedBuildRegistry(
         includedBuildFactory,
         Stub(IncludedBuildDependencySubstitutionsBuilder),
